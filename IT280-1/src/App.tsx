7683a720067ae4b29c9e2a689f072391ee5e2b48
aaryanneupane <aaryan200513@gmail.com>
2023-09-04 14:38:57 +0200

    Added a routes file

--- IT280-1/src/App.tsx
+++ IT280-1/src/App.tsx
@@ -1,19 +1,14 @@
-import { useState } from 'react';
-import { Routes, Route } from "react-router-dom";
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
+import React from 'react';
 import './App.css';
-import HomePage from './pages/HomePage';
+import AppRoutes from './routes';
 
 function App() {
 
 
   return (
     <div>
-    <Routes>
-      <Route path="/" element={<HomePage />} />
-    </Routes>
-  </div>
+      <AppRoutes/>
+    </div>
   );
 }
 
