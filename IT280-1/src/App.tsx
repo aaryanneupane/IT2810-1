af3d94ba2d6f0b4be657ebfbc20470a3353d93bc
aaryanneupane <aaryan200513@gmail.com>
2023-09-05 12:07:25 +0200

    Installed and ran prettier and added default messages to merge and issues.

--- IT280-1/src/App.tsx
+++ IT280-1/src/App.tsx
@@ -1,18 +1,22 @@
-import React from 'react';
-import { Route, createBrowserRouter, createRoutesFromElements, RouterProvider } from 'react-router-dom';
-import HomePage from './pages/HomePage';
-import FavouritePage from './pages/FavouritePage';
+import React from "react";
+import {
+  Route,
+  createBrowserRouter,
+  createRoutesFromElements,
+  RouterProvider,
+} from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import FavouritePage from "./pages/FavouritePage";
 
 function App() {
-  
   const router = createBrowserRouter(
     createRoutesFromElements(
       <>
         <Route path="/" element={<HomePage />} />
         <Route path="home" element={<HomePage />} />
         <Route path="favourite" element={<FavouritePage />} />
-      </>
-    )
+      </>,
+    ),
   );
 
   return (
