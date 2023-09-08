0b601715faa86d99fe5a927d76f0eea0ce74686e
ingrid tandberg ryan <ingritry@stud.ntnu.no>
2023-09-08 13:46:20 +0200

    Added logo, edited colors, added button

--- IT280-1/src/components/Header/Header.tsx
+++ IT280-1/src/components/Header/Header.tsx
@@ -1,6 +1,19 @@
-import React from "react";
+import MyButton from "../Button/Button";
 import styles from "./Header.module.css";
+import myImage from './myLogo.png';
+
 
 export default function Header() {
-  return <div className={styles.header}></div>;
+  return( 
+    <div className={styles.header}>
+      <img
+        className={styles.image}
+        src={myImage} 
+        alt="My Logo" 
+      />
+    
+    <MyButton></MyButton>
+    
+    </div>
+  );
 }
--- IT280-1/src/pages/HomePage.tsx
+++ IT280-1/src/pages/HomePage.tsx
@@ -4,15 +4,13 @@ import Button from "../components/Button/Button";
 
 const HomePage = () => {
   const [count, setCount] = useState(0);
-  
 
   return (
     <div style={{ width: "100vw" }}>
       <Header></Header>
       <div>
-        <h1>valuta gutta</h1>
       </div>
-      <Button></Button>
+
     </div>
   );
 };
